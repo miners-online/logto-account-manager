5a9df5fc03609833ad3028e6866c6a862edbc537
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-02-27 19:00:58 +0000

    feat: add UI components and hooks for user interface

--- app/page.tsx
+++ app/page.tsx
@@ -2,62 +2,112 @@ import { getLogtoContext, signIn, signOut } from '@logto/next/server-actions';
 import { logtoConfig } from '@/lib/logto';
 import { ServerActionButton } from '@/components/server-action-button';
 
-export default async function Home() {
+import { AppSidebar } from "@/components/app-sidebar"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+
+// export default async function Home() {
+//   const { isAuthenticated, claims } = await getLogtoContext(logtoConfig);
+
+//   return (
+//     <>
+//       <nav>
+//         {isAuthenticated ? (
+//           <span>
+//             Hello, {claims?.sub},
+//             <ServerActionButton
+//               onClick={async () => {
+//                 'use server';
+
+//                 await signOut(logtoConfig);
+//               }}
+//             >
+//               Sign out
+//             </ServerActionButton>
+//           </span>
+//         ) : (
+//           <span>
+//             <ServerActionButton
+//               onClick={async () => {
+//                 'use server';
+
+//                 await signIn(logtoConfig);
+//               }}
+//             >
+//               Sign in
+//             </ServerActionButton>
+//           </span>
+//         )}
+//       </nav>
+//       <main>
+//       {claims && (
+//         <div>
+//           <h2>Claims:</h2>
+//           <table>
+//             <thead>
+//               <tr>
+//                 <th>Name</th>
+//                 <th>Value</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {Object.entries(claims).map(([key, value]) => (
+//                 <tr key={key}>
+//                   <td>{key}</td>
+//                   <td>{String(value)}</td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
+//         </div>
+//       )}
+//     </main>
+//     </>
+//   )
+// }
+
+export default async function Page() {
   const { isAuthenticated, claims } = await getLogtoContext(logtoConfig);
 
   return (
-    <>
-      <nav>
-        {isAuthenticated ? (
-          <span>
-            Hello, {claims?.sub},
-            <ServerActionButton
-              onClick={async () => {
-                'use server';
-
-                await signOut(logtoConfig);
-              }}
-            >
-              Sign out
-            </ServerActionButton>
-          </span>
-        ) : (
-          <span>
-            <ServerActionButton
-              onClick={async () => {
-                'use server';
-
-                await signIn(logtoConfig);
-              }}
-            >
-              Sign in
-            </ServerActionButton>
-          </span>
-        )}
-      </nav>
-      <main>
-      {claims && (
-        <div>
-          <h2>Claims:</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(claims).map(([key, value]) => (
-                <tr key={key}>
-                  <td>{key}</td>
-                  <td>{String(value)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          {claims && (
+            <div>
+              <h2>Claims:</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.entries(claims).map(([key, value]) => (
+                    <tr key={key}>
+                      <td>{key}</td>
+                      <td>{String(value)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
-      )}
-    </main>
-    </>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,103 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Your Settings",
+      url: "#",
+      icon: Settings2,
+      isActive: true,
+      items: [
+        {
+          title: "Personal Info",
+          url: "#",
+        },
+        {
+          title: "Sign-in & Security",
+          url: "#",
+        },
+        {
+          title: "Privacy Settings",
+          url: "#",
+        },
+        {
+          title: "Connections",
+          url: "#",
+        },
+        {
+          title: "Your Data",
+          url: "#",
+        },
+        {
+          title: "Account Actions",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
